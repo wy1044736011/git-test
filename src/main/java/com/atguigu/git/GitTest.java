5c83d66a28f4f377df9993b96cab6c737ffc5ab4
石头人 <1044716011@qq.com>
2022-08-01 20:36:32 +0800

    新建

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * title:
+ *
+ * @author ROG
+ * date 2022/8/1
+ * description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
